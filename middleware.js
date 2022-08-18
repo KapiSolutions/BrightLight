513ab97897c17479b1cf13b3066adf1a2655419e
JKapturkiewicz <kuba.kapek@gmail.com>
2022-08-18 11:37:28 +0200

    middleware test3

--- middleware.js
+++ middleware.js
@@ -1,22 +1,24 @@
 // eslint-disable-next-line @next/next/no-server-import-in-page
 import { NextResponse } from 'next/server'
+// import absoluteUrl from 'next-absolute-url'
 
 export default function middleware(req){
     // const publicRoutes = ['/','/about','/sign-in','/register','/404']
-    // const origin = process.env.VERCEL_URL
+    // const { origin } = absoluteUrl(req)
+    // const origin = 'http://localhost:3000'
     const origin = 'https://bright-light.vercel.app'
-    console.log('origin: ', origin)
     const verify = !!req.cookies.get('userLoggedIn')
     const url = req.url
     const path = url.replace(origin, '') 
-    
+    console.log('verify: ', verify)
+    // console.log('origin: ', origin)
 
-    if(!verify  && !path.includes('.')){
+    if(!verify && !path.includes('.')){
         console.log('restricted')
         return NextResponse.redirect(`${origin}/sign-in`)
     }
 
-    return NextResponse.next()
+    // return NextResponse.next()
 }
 
 export const config = {
