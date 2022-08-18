d17bdc8a75dd616ef4a56dfda2d9b47ab21451a5
JKapturkiewicz <kuba.kapek@gmail.com>
2022-08-18 12:24:49 +0200

    middleware test4

--- middleware.js
+++ middleware.js
@@ -2,26 +2,30 @@
 import { NextResponse } from 'next/server'
 // import absoluteUrl from 'next-absolute-url'
 
-export default function middleware(req){
+export default async function middleware(req){
     // const publicRoutes = ['/','/about','/sign-in','/register','/404']
     // const { origin } = absoluteUrl(req)
     // const origin = 'http://localhost:3000'
-    const origin = 'https://bright-light.vercel.app'
-    const verify = !!req.cookies.get('userLoggedIn')
-    const url = req.url
-    const path = url.replace(origin, '') 
-    console.log('verify: ', verify)
-    // console.log('origin: ', origin)
+    // const origin = 'https://bright-light.vercel.app'
+    // const verify = !!req.cookies.get('userLoggedIn')
+    // const url = req.url
+    // const path = url.replace(origin, '') 
+    // console.log('verify: ', verify)
+    // // console.log('origin: ', origin)
 
-    if(!verify && !path.includes('.')){
-        console.log('restricted')
-        return NextResponse.redirect(`${origin}/sign-in`)
-    }
+    // if(!verify && !path.includes('.')){
+    //     console.log('restricted')
+    //     return NextResponse.redirect(`${origin}/sign-in`)
+    // }
 
+    if (req.nextUrl.pathname.startsWith('/blog')) {
+        const authCookie = req.cookies.get('userLoggedIn');
+        if (!authCookie) return NextResponse.redirect(new URL('/sign-in', req.url));
+      }
     // return NextResponse.next()
 }
 
-export const config = {
-    matcher: ['/blog/:path*']
-    // matcher: ['/blog/:path*', '/api/admin/:path*']
-};
+// export const config = {
+//     matcher: ['/blog/:path*']
+//     // matcher: ['/blog/:path*', '/api/admin/:path*']
+// };
