007a12e9932fbfeb932d647f8645e70bd1d124fc
JKapturkiewicz <kuba.kapek@gmail.com>
2022-08-18 11:12:39 +0200

    middleware test2

--- middleware.js
+++ middleware.js
@@ -3,18 +3,18 @@ import { NextResponse } from 'next/server'
 
 export default function middleware(req){
     // const publicRoutes = ['/','/about','/sign-in','/register','/404']
-    const origin = process.env.VERCEL_URL
-    // const origin = 'https://bright-light.vercel.app'
+    // const origin = process.env.VERCEL_URL
+    const origin = 'https://bright-light.vercel.app'
     console.log('origin: ', origin)
-    // const verify = !!req.cookies.get('userLoggedIn')
-    // const url = req.url
-    // const path = url.replace(origin, '') 
+    const verify = !!req.cookies.get('userLoggedIn')
+    const url = req.url
+    const path = url.replace(origin, '') 
     
 
-    // if(!verify  && !path.includes('.')){
-    //     console.log('restricted')
-    //     return NextResponse.redirect(`${origin}/sign-in`)
-    // }
+    if(!verify  && !path.includes('.')){
+        console.log('restricted')
+        return NextResponse.redirect(`${origin}/sign-in`)
+    }
 
     return NextResponse.next()
 }
