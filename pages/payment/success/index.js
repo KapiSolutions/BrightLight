30d754ca7a132bf682999d8a872bb59d9256cb94
JKapturkiewicz <kuba.kapek@gmail.com>
2022-12-31 13:14:06 +0100

    update styling

--- pages/payment/success/index.js
+++ pages/payment/success/index.js
@@ -25,8 +25,8 @@ function SuccessPaymentPage() {
       <Head>
         <title>BrightLight | Payment complete</title>
       </Head>
-      <Container className="justify-content-center text-center mt-5" id="ps-ctx">
-        <h2 className="color-primary">Payment complete!</h2>
+      <Container className="justify-content-center text-center mt-5 color-primary" id="ps-ctx">
+        <h2>Payment complete!</h2>
         <IoCheckmarkDoneCircleOutline style={{width: "40px", height: "40px"}} className="mb-3"/>
         <p className="color-primary">Check your email for the order confirmation.</p> 
         <p className="color-primary">You can check also the status of your order and all other details below:</p>      
