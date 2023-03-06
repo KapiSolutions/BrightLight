9413d5046ce074f9bf116c52269c78cfd6e540b4
JKapturkiewicz <kuba.kapek@gmail.com>
2023-03-06 20:04:34 +0100

    Add comment with adsense script

--- pages/index.js
+++ pages/index.js
@@ -26,6 +26,8 @@ export default function Home(props) {
     <>
       <Head>
         <title>BrightLight | {t[locale].title}</title>
+        {/* <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6416874611042159"
+     crossorigin="anonymous"></script> */}
       </Head>
 
       <Script
