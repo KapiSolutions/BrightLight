6af34f0c99f2eeddc8071adffc89eab22c9cc959
JKapturkiewicz <kuba.kapek@gmail.com>
2023-03-10 10:33:14 +0100

    Add cookies policy

--- pages/cookies-policy/index.js
+++ pages/cookies-policy/index.js
@@ -1,19 +1,52 @@
 import React from "react";
 import Head from "next/head";
 import { Container } from "react-bootstrap";
+import path from "path";
+import { promises as fs } from "fs";
+const parse = require("html-react-parser");
+import DOMPurify from "dompurify";
 
-function CookiesPolicy() {
+function CookiesPolicyPage(props) {
+  const locale = props.locale;
+  const t = {
+    en: {
+      title: "Cookies Policy",
+
+    },
+    pl: {
+      title: "Polityka Cookies",
+
+    },
+  };
   return (
     <>
       <Head>
-        <title>BrightLight | Cookies Policy</title>
+        <title>BrightLight | {t[locale].title}</title>
       </Head>
       <Container className="justify-content-center text-center mt-5">
-        <h1 className="color-primary">Cookies Policy</h1>
-        <p className="color-primary">Coming soon. Site still in the developer version.</p>
+        <h1 className="color-primary">{t[locale].title}</h1>
+        <section className="text-start ps-1 pe-1">{parse(DOMPurify.sanitize(props.text))}</section>
       </Container>
     </>
   );
 }
 
-export default CookiesPolicy;
+export default CookiesPolicyPage;
+
+export async function getStaticProps({ locale }) {
+  let filePath = "";
+  let text = "";
+  
+    if (locale != "default") {
+      filePath = path.join(process.cwd(), `public/regulations/cookies/${locale}/index.txt`);
+      text = await fs.readFile(filePath, "utf8");
+    } 
+      return {
+        props: {
+          text: text,
+          locale: locale,
+        },
+        revalidate: false, //on demand revalidation
+      };
+    
+  }
