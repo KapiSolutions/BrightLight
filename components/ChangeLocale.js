30e79eac1b33c089eb3ba658702686a338407b73
JKapturkiewicz <kuba.kapek@gmail.com>
2023-03-10 16:03:15 +0100

    Change currency on the language changes - temporary

--- components/ChangeLocale.js
+++ components/ChangeLocale.js
@@ -1,13 +1,16 @@
 import { useRouter } from "next/router";
 import React from "react";
+import { useDeviceStore } from "../stores/deviceStore";
 
 function ChangeLocale() {
   const router = useRouter();
   const locale = router.locale;
+  const setCurrency = useDeviceStore((state) => state.setCurrency);
 
   const toggleLanguage = (newLocale) => {
     const { pathname, asPath, query } = router;
     router.push({ pathname, query }, asPath, { locale: newLocale });
+    setCurrency(newLocale == "pl" ? "pln" : "usd");
   };
   return (
     <span
