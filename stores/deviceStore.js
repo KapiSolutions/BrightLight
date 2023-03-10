95a218f0543ab74e0c1971ecd1c4733633f00f76
JKapturkiewicz <kuba.kapek@gmail.com>
2023-03-10 15:43:16 +0100

    Delete language variable

--- stores/deviceStore.js
+++ stores/deviceStore.js
@@ -3,11 +3,9 @@ import create from 'zustand'
 export const useDeviceStore = create((set) => ({
   isMobile: false,
   themeState: "light",
-  lang: "en",
   currency: "usd",
   setMobile: (state) => set({ isMobile: state }),
   setThemeState: (state) => set({ themeState: state }),
-  setLang: (state) => set({ lang: state }),
   setCurrency: (state) => set({ currency: state }),
 }))
 
