2a56086a2fbfa36e51451c877583a07c1859f181
JKapturkiewicz <kuba.kapek@gmail.com>
2023-03-10 11:12:54 +0100

    Add privacy policy

--- pages/privacy-policy/index.js
+++ pages/privacy-policy/index.js
@@ -1,19 +1,52 @@
 import React from "react";
 import Head from "next/head";
 import { Container } from "react-bootstrap";
+import path from "path";
+import { promises as fs } from "fs";
+const parse = require("html-react-parser");
+import DOMPurify from "dompurify";
 
-function PrivacyPolicy() {
+function PrivacyPolicy(props) {
+  const locale = props.locale;
+  const t = {
+    en: {
+      title: "Privacy Policy",
+
+    },
+    pl: {
+      title: "Polityka Prywatności",
+
+    },
+  };
   return (
     <>
       <Head>
-        <title>BrightLight | Privacy Policy</title>
+        <title>BrightLight | {t[locale].title}</title>
       </Head>
-      <Container className="justify-content-center text-center mt-5">
-        <h1 className="color-primary">Privacy Policy</h1>
-        <p className="color-primary">Coming soon. Site still in the developer version.</p>
+      <Container className="justify-content-center text-center mt-5" style={{maxWidth: "100vw"}}>
+        <h1 className="color-primary">{t[locale].title}</h1>
+        <section className="text-start ps-1 pe-1">{parse(DOMPurify.sanitize(props.text))}</section>
       </Container>
     </>
   );
 }
 
 export default PrivacyPolicy;
+
+export async function getStaticProps({ locale }) {
+  let filePath = "";
+  let text = "";
+  
+    if (locale != "default") {
+      filePath = path.join(process.cwd(), `public/regulations/privacy/${locale}/index.txt`);
+      text = await fs.readFile(filePath, "utf8");
+    } 
+      return {
+        props: {
+          text: text,
+          locale: locale,
+        },
+        revalidate: false, //on demand revalidation
+      };
+    
+  }
