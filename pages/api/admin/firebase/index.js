db826790aab1e51967e77bb87b7a38972c2d3b1d
JKapturkiewicz <kuba.kapek@gmail.com>
2023-03-13 18:49:57 +0100

    Update firebase admin api

--- pages/api/admin/firebase/index.js
+++ pages/api/admin/firebase/index.js
@@ -11,25 +11,47 @@ export default async function orderFinishEmail(req, res) {
       return res.status(401).end("Invalid token");
     }
 
+    //Initialize app only when not already initialized
     try {
       if (admin.apps.length === 0) {
         admin.initializeApp({
           credential: admin.credential.cert(serviceAccount),
         });
       }
-
-      //* Get user:
-      // const user = await admin.auth().getUser(uid);
-
-      // Delete user:
-      if (type === "delete") {
-        await admin.auth().deleteUser(data.uid);
-      }
-
-      return res.status(200).end("OK");
     } catch (error) {
       return res.status(500).send(error);
     }
+    const auth = admin.auth();
+    const db = admin.firestore();
+
+    // Handle Firebase Api requests
+    switch (type) {
+      case "delete-user":
+        try {
+          await auth.deleteUser(data.uid);
+          res.status(200).end("OK");
+        } catch (e) {
+          res.status(500).send(e);
+        }
+        break;
+      case "create-doc":
+        try {
+          await db
+            .collection("test")
+            .doc("/" + data.id + "/")
+            .create({ id: data.id, test1: data.item });
+          res.status(200).end("OK");
+        } catch (e) {
+          res.status(500).send(e);
+        }
+        break;
+
+      default:
+        break;
+    }
+
+    // Get user:
+    // const user = await admin.auth().getUser(uid);
   } else {
     res.setHeader("Allow", "POST");
     res.status(405).end("Method Not Allowed");
