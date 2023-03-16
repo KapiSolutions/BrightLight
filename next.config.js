275a2e545cb7d044a3d585ee8a8932d4966a8921
JKapturkiewicz <kuba.kapek@gmail.com>
2023-03-16 21:51:13 +0100

    Update CSP

--- next.config.js
+++ next.config.js
@@ -33,7 +33,7 @@ const nextConfig = {
           {
             key: "Content-Security-Policy",
             value:
-              "default-src 'self'; form-action 'self'; style-src 'self'; script-src 'self' https://firestore.googleapis.com https://google-analytics.com https://ajax.googleapis.com https://googletagmanager.com;",
+              "default-src 'self'; form-action 'self'; style-src 'self'; script-src 'self' https://firestore.googleapis.com https://securetoken.googleapis.com https://google-analytics.com https://ajax.googleapis.com https://googletagmanager.com;",
           },
           {
             key: "X-Content-Type-Options",
