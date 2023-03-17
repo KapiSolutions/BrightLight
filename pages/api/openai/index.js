18103590d6e44ab52a68a4d2413d92b674438488
JKapturkiewicz <kuba.kapek@gmail.com>
2023-03-17 09:32:21 +0100

    Create openai Api

---
+++ pages/api/openai/index.js
@@ -0,0 +1,56 @@
+const { Configuration, OpenAIApi } = require("openai");
+import { auth } from "../../../config/firebaseAdmin";
+import { csrf } from "../../../config/csrf";
+import verifyRequest from "../../../utils/verifyRequest";
+// https://platform.openai.com/docs/api-reference/chat/create?lang=node.js
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+async function openAi(req, res) {
+  if (req.method === "POST") {
+    const { secret, idToken, data } = req.body;
+
+    // Verify data
+    const question = data || "";
+    if (question.trim().length === 0 || question.trim().length > 200) {
+      res.status(400).json({
+        error: {
+          message: "Please enter a valid question (max 200 characters)",
+        },
+      });
+      return;
+    }
+    // Verify request
+    const uid = await verifyRequest(auth, secret, idToken, req, res);
+    if (uid) {
+      try {
+        const completion = await openai.createChatCompletion({
+          model: "gpt-3.5-turbo",
+          messages: [{ role: "user", content: question }],
+        });
+        console.log(completion.data.choices[0].message);
+        res.status(200).json({ answer: completion.data.choices[0].message });
+      } catch (err) {
+        if (err.response) {
+          console.error(err.response.status, err.response.data);
+          res.status(err.response.status).json(err.response.data);
+        } else {
+          console.error(`Error with OpenAI API request: ${err.message}`);
+          res.status(500).json({
+            error: {
+              message: "An error occurred during your request.",
+            },
+          });
+        }
+      }
+    }
+  } else {
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method Not Allowed");
+  }
+}
+
+export default csrf(openAi);
