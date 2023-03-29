ff2b688134e4ce311d918ed0f978ec16493caddf
JKapturkiewicz <kuba.kapek@gmail.com>
2023-03-29 11:34:37 +0200

    Disable LogRocket

--- pages/_app.js
+++ pages/_app.js
@@ -8,8 +8,8 @@ import AuthProvider from "../context/AuthProvider";
 import { useDeviceStore } from "../stores/deviceStore";
 import { useRouter } from "next/router";
 import { GoogleAnalytics } from "nextjs-google-analytics";
-import LogRocket from "logrocket";
-import setupLogRocketReact from "logrocket-react";
+// import LogRocket from "logrocket";
+// import setupLogRocketReact from "logrocket-react";
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
