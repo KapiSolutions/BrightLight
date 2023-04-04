c9257c46625146b474d883bb15c1ea7f2e35671a
JKapturkiewicz <kuba.kapek@gmail.com>
2023-04-04 14:50:03 +0200

    Update translations

--- components/HowItWorks.js
+++ components/HowItWorks.js
@@ -8,26 +8,24 @@ function HowItWorks(props) {
   const t = {
     en: {
       h: "How it works?",
-      p1: `Our platform allows you to receive insightful Online Tarot readings from professional esotericist Bright Light Gypsy, 
-            or to utilize our state-of-the-art artificial intelligence technology for a more affordable option.`,
-      p2: `Thanks to the algorithms used, you can feel like at a meeting with a real Tarot reader, all without leaving your home!
-             You choose the cards, so you have a real impact on the result of your own divination and, as a result, the advice you receive.`,
-      p3: `We offer an easy to use interface where you can buy coins to use towards your AI readings and
-            receive personalized guidance on various aspects of life, including love, career, and personal growth. With the
-            option to choose between a human or AI tarot reader, you can find the perfect fit for your needs and budget.`,
-      p4: `Don't wait and get Your accurate and empowering readings that will help guide you on your journey!`,
+      p1: `It's Easy! Just choose your cards, ask a question, and receive an interpretation from our expert esotericist, Bright Light Gypsy, or through our advanced AI technology. ❤`,
+      p2: `Our algorithms are designed to provide a personalized and authentic experience, making you feel like you are in a one-on-one meeting with a real Tarot reader, all from the 
+            comfort of your own home. With the ability to select your own cards, you have a real impact on the outcome of your divination, allowing you to receive tailored advice on various 
+            aspects of life, including love, career, and personal development.`,
+      p3: `Our user-friendly interface allows you to purchase coins to use for your AI readings, and you can easily exchange them for personalized tips. Choose between an interpretation by 
+            our experienced esotericist or our AI technology, depending on your needs and budget.`,
+      p4: `Don't hesitate to take advantage of our services to gain insights and clarity on your life path. Join us on this beautiful journey and discover the guidance you need to live your best life!`,
     },
     pl: {
       h: "Jak to działa?",
-      p1: `To prostę, wybierz swoje karty, zadaj pytanie i otrzymaj interpretację! Nasza platforma dostarcza szereg 
-      prywatnych interpretacji Tarota i porad za pośrednictwem ezoteryczki Bright Light Gypsy,
-      lub poprzez technologię sztucznej inteligencji AI.`,
-      p2: `Dzięki zastowowanym algorytmom możesz poczuć się jak na spotkaniu z prawdziwą Tarocistką, a to wszystko bez wychodzenia z domu! 
-      To Ty wybierasz karty więc masz realny wpływ na wynik własnej wróżby i w efekcie uzyskanej porady.`,
-      p3: `Oferujemy łatwy w użyciu interfejs, w którym możesz kupować monety do wykorzystania w swoich odczytach AI.
-      Monety w prosty sposób wymienisz na spersonalizowane wskazówki dotyczące różnych aspektów życia, w tym miłości, kariery i rozwoju osobistego.
-      Dzięki możliwość wyboru między interpretacją przez doświadczoną ezoteryczkę lub sztuczną inteligencję, znajdziesz idealne dopasowanie do swoich potrzeb i budżetu.`,
-      p4: `Nie czekaj i uzyskaj dokładne i budujące odczyty, które pomogą Ci w tej pięknej podróży jaką jest życie!`,
+      p1: `To łatwe! Po prostu wybierz swoje karty, zadaj pytanie i uzyskaj interpretację od profesjonalnej ezoteryczki Bright Light Gypsy, 
+          lub za pośrednictwem naszej zaawansowanej technologii AI. ❤`,
+      p2: `Nasze algorytmy zostały zaprojektowane tak, aby zapewnić spersonalizowane i autentyczne doświadczenie, zupełnie jak na spotkaniu z prawdziwą Tarocistką, 
+            a to wszystko bez wychodzenia z domu! Dzięki możliwości doboru własnych kart masz realny wpływ na wynik swoich wróżb, i w efekcie uzyskanej porady.`,
+      p3: `Nasz przyjazny dla użytkownika interfejs umożliwia zakup monet do wykorzystania w odczytach AI, a także łatwą wymianę ich na spersonalizowane wskazówki 
+      dotyczące różnych aspektów życia, w tym miłości, kariery i rozwoju osobistego.
+      Dzięki możliwość wyboru między interpretacją od doświadcznej ezoteryczki lub naszej technologii AI, znajdziesz idealne dopasowanie do swoich potrzeb i budżetu.`,
+      p4: `Nie czekaj i uzyskaj dokładne i spersonalizowane odczyty, które pomogą Ci w tej pięknej podróży jaką jest życie!`,
     },
   };
   return (
@@ -37,7 +35,7 @@ function HowItWorks(props) {
           <Image src={avatarPath} width="170" height="170" alt="Avatar" />
         </div>
         <h2 className="text-center">{t[locale].h}</h2>
-        <div className={`${isMobile ? "w-100" : "w-75"} m-auto`} style={{textAlign: "justify"}}>
+        <div className={`${isMobile ? "w-100" : "w-75"} m-auto`} style={{ textAlign: "justify" }}>
           <p>{t[locale].p1}</p>
           <p>{t[locale].p2}</p>
           <p>{t[locale].p3}</p>
