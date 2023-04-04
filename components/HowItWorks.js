56f38ffc5525a717c1ea6b92d7626155a77788bf
JKapturkiewicz <kuba.kapek@gmail.com>
2023-04-04 23:22:10 +0200

    Change scroll point on the mobile

--- components/HowItWorks.js
+++ components/HowItWorks.js
@@ -82,7 +82,7 @@ function HowItWorks(props) {
             onClick={() => {
               document
                 .getElementsByName("howToSectionDescription")[0]
-                .scrollIntoView({ block: isMobile ? "end" : "center", inline: "nearest" });
+                .scrollIntoView({ block: isMobile ? "start" : "center", inline: "nearest" });
             }}
           >
             {t[locale].button}
